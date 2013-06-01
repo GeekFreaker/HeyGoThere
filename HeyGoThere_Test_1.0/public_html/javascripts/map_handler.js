696eab81edc13aa040ebe7b4ceb376af21a9d11c
Raoul DIFFOUO <diraulo@gmail.com>
2013-06-01 21:58:41 +0200

    post user details to server now working

--- HeyGoThere_Test_1.0/public_html/javascripts/map_handler.js
+++ HeyGoThere_Test_1.0/public_html/javascripts/map_handler.js
@@ -4,11 +4,8 @@
  * 
  */
 
-var icon = new google.maps.MarkerImage("./images/map_markers/hospital-building.png",
-           new google.maps.Size(32, 32), new google.maps.Point(0, 0),
-           new google.maps.Point(16, 32));
-
 var center_africa = new google.maps.LatLng(1.757537, 18.281250);
+var lesotho = new google.maps.LatLng(-29.5495, 28.1851);
 var map = null;
 var currentPopup;
 
@@ -17,54 +14,17 @@ var markers = [];
 var locations = [];
 var iterator = 0;
 
-function createLocations(lat, lng) {
-    //confirm("bonjour : Lat: '" + lat + "', Lon: '"+ lng +" '");
-    locations.push(new google.maps.LatLng(lat, lng));
-    //window.confirm("bonjour : Lat: '" + lat + "', Lon: '"+ lng +" '");
+var map;
+
+function initialize() {
+  var mapOptions = {
+    zoom: 9,
+    center: lesotho,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+  
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
 }
 
-function addMarker() {
-    //confirm("Adding Markers");
-    markers.push(new google.maps.Marker({
-        position: locations[iterator],
-        icon: icon,
-        draggable: false,
-        map: map,
-        animation: google.maps.Animation.DROP
-    }));
-    iterator++;
-}
-
-// Sets the map on all markers in the array.
-function setAllMap(map) {
-    //confirm("Set Markerssss " + markers.length);
-    for (var i = 0; i < markers.length; i++) {
-      //confirm("Set Markers");
-      markers[i].setMap(map);
-    }
-}  
-
-function dropMarkers() {
-  //confirm("Set Markers");
-  setAllMap(null);
-  setAllMap(map);
-  for (var i = 0; i < locations.length; i++) {
-      setTimeout(function() {
-          addMarker();
-      }, i * 10);                            
-  }
-}
-
-function initMap() {
-    //confirm("Initialising . . .");
-    var mapOptions = {
-        center: center_africa,
-        zoom: 4,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    //setAllMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initMap);
+google.maps.event.addDomListener(window, 'load', initialize);
